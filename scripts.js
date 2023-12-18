578843280eb403439c96f26caba1d73a884caa86
TheManBehindTheBananas <154103339+TheManBehindTheBananas@users.noreply.github.com>
2023-12-18 09:29:10 +0800

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,12 +1,10 @@
-// Function to add a task
 function addTask() {
   const taskInput = document.getElementById('taskInput').value;
-  const importance = document.getElementById('importanceSlider').value;
-  const urgency = document.getElementById('urgencySlider').value;
+  const importance = parseInt(document.getElementById('importanceSlider').value);
+  const urgency = parseInt(document.getElementById('urgencySlider').value);
 
   const taskList = document.getElementById('taskList');
 
-  // Create list item
   const li = document.createElement('li');
   li.innerHTML = `
     <span>${taskInput}</span>
@@ -14,10 +12,8 @@ function addTask() {
     <button onclick="deleteTask(this)">X</button>
   `;
 
-  // Calculate importance/urgency ratio
   const ratio = importance / urgency;
 
-  // Insert task based on calculated ratio
   let inserted = false;
   const tasks = taskList.getElementsByTagName('li');
   for (let i = 0; i < tasks.length; i++) {
@@ -35,11 +31,9 @@ function addTask() {
     taskList.appendChild(li);
   }
 
-  // Set data attribute for sorting
   li.setAttribute('data-ratio', ratio);
 }
 
-// Function to delete a task
 function deleteTask(button) {
   const task = button.parentNode;
   task.parentNode.removeChild(task);
